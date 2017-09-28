9cc939e5b5cec7741af6f38af99aa983e6e5accb
jasonburma <jason.burma@drake.edu>
2017-09-27 20:42:51 -0500

    Dis be Tutorial 4 rough

---
+++ app/src/main/java/burmacodes/bootifulbulldawg/PagerAdapter.java
@@ -0,0 +1,41 @@
+package burmacodes.bootifulbulldawg;
+
+/**
+ * Created by Jason on 9/27/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter{
+
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                BulldogListFragment tab1 = new BulldogListFragment();
+                return tab1;
+            case 1:
+                RankingsFragment tab2 = new RankingsFragment();
+                return tab2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+
+
+}
